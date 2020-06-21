5633a2af6e927853b88e5bec58fc275d09fafc06
Aakriti Sharma <aakriti01as@gmail.com>
2020-06-21 21:41:12 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# STAT-DISTRIBUTIONS
+This package was build under guided project based course by AWS at Udacity.
+The package is for calculating and visualizing Gaussian and Binomial Probability distribution.
+
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='stats_distributions_aws',
+      version='0.1',
+      description='Gaussian Distributions',
+      packages=['stats_distributions_aws'],
+      zip_safe=False)
